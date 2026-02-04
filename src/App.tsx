fb3d02e36ebc0c1ec196402f6c807561a88b5172
deepp5 <deepnpatel05@gmail.com>
2026-02-04 12:07:51 -0600

    Removed the comment

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/chat/:chatId" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
